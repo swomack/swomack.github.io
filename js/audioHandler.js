455304143a6fd569269b37181023bf436a8acd70
ENOSISBD\sourov <rasheduzzaman.sourov@enosisbd.com>
2019-08-26 18:46:15 +0600

    Added sound on click

---
+++ js/audioHandler.js
@@ -0,0 +1,15 @@
+// AFRAME.registerComponent('audiohandler', {
+//     init: function () {
+//       let playing = false;
+//       let audio = document.querySelector("#audio");
+//       this.el.addEventListener('click', () => {
+//         if (!playing) {
+//           audio.play();
+//         } else {
+//           audio.pause();
+//           audio.currentTime = 0;
+//         }
+//         playing = !playing;
+//       });
+//     }
+//   })
